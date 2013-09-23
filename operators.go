d9dbe2a598dec93857cf4a1f2569b34505f48aea
Jesse van den Kieboom <jessevdk@gnome.org>
2013-09-23 18:41:14 +0200

    Allow parsing of single files

--- operators.go
+++ operators.go
@@ -13,12 +13,7 @@ import (
 	"path"
 )
 
-func getSourceDir(args []string) string {
-	if len(args) > 1 {
-		fmt.Fprintf(os.Stderr, "Please provide only a single directory to parse\n")
-		os.Exit(1)
-	}
-
+func getSources(args []string) (dirname string, files []string) {
 	if len(args) == 0 {
 		dirname, err := os.Getwd()
 
@@ -27,16 +22,30 @@ func getSourceDir(args []string) string {
 			os.Exit(1)
 		}
 
-		return dirname
+		return dirname, nil
 	}
 
-	return args[0]
+	if len(args) == 1 {
+		info, err := os.Stat(args[0])
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to check file `%s': %s\n", args[0], err)
+			os.Exit(1)
+		}
+
+		if info.IsDir() {
+			return args[0], nil
+		}
+	}
+
+	return "", args
 }
 
 func main() {
 	var opts struct {
 		Output  string `short:"o" long:"output" description:"Output directory (required)" required:"true"`
 		Verbose bool   `short:"v" long:"verbose" description:"Enable verbose mode"`
+		Package string `short:"p" long:"package" description:"Package name" default:"main"`
 	}
 
 	fp := flags.NewParser(&opts, flags.Default)
@@ -53,19 +62,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	dirname := getSourceDir(args)
+	dirname, files := getSources(args)
 
-	p, err := build.ImportDir(dirname, 0)
+	if len(dirname) != 0 {
+		p, err := build.ImportDir(dirname, 0)
 
-	if err != nil {
-		panic(err)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, f := range p.GoFiles {
+			files = append(files, path.Join(dirname, f))
+		}
 	}
 
 	fs := token.NewFileSet()
-	afs := make([]*ast.File, 0, len(p.GoFiles))
+	afs := make([]*ast.File, 0, len(files))
 
-	for _, f := range p.GoFiles {
-		af, err := parser.ParseFile(fs, path.Join(dirname, f), nil, 0)
+	for _, f := range files {
+		af, err := parser.ParseFile(fs, f, nil, 0)
 
 		if err != nil {
 			panic(err)
@@ -74,7 +89,13 @@ func main() {
 		afs = append(afs, af)
 	}
 
-	pp, err := types.Check(dirname, fs, afs)
+	pkgname := path.Base(dirname)
+
+	if len(pkgname) == 0 {
+		pkgname = opts.Package
+	}
+
+	pp, err := types.Check(pkgname, fs, afs)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error while parsing: %s\n", err)
@@ -117,7 +138,13 @@ func main() {
 			return call
 		}, f).(*ast.File)
 
-		fn := path.Join(opts.Output, p.GoFiles[i])
+		fn := path.Join(opts.Output, files[i])
+		err := os.MkdirAll(path.Join(opts.Output, path.Dir(files[i])), 0766)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create output directory: %s\n", err)
+			os.Exit(1)
+		}
 
 		of, err := os.Create(fn)
 
